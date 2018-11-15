abaa04ba79fc3b0f4cfd146447f318e35e153bce
Travis Fischer <fisch0920@gmail.com>
2018-11-15 11:15:20 +0000

    .

--- index.js
+++ index.js
@@ -1,47 +1,20 @@
 'use strict'
 
-const url = 'https://www.menuwithprice.com/menu/wahlburgers/'
-const cheerio = require('cheerio')
-const request = require('request-promise-native')
-
 /**
- * Gets dem Wahlburger burgers.
+ * TODO
  *
- * @name getBurgers
+ * @name silvi
  * @type function
  *
  * @return Promise
  */
 module.exports = async () => {
-  const html = await request(url)
-  const $ = cheerio.load(html)
-
-  const getBurger = (i, el) => {
-    const $tds = $(el).find('td')
-      .map((i, el) => $(el).text())
-
-    return {
-      name: $tds[0],
-      price: $tds[1],
-      desc: $tds[2]
-    }
-  }
-
-  const burgers0 = $('table.prc-table:nth-child(4) tr')
-    .map(getBurger)
-    .get()
-
-  const burgers1 = $('table.prc-table:nth-child(6) tr')
-    .map(getBurger)
-    .get()
-
-  const burgers = burgers0.concat(burgers1)
-  return burgers
+  // TODO
 }
 
 if (!module.parent) {
   module.exports()
-    .then((burgers) => {
-      console.log(JSON.stringify(burgers, null, 2))
+    .then((result) => {
+      console.log(JSON.stringify(result, null, 2))
     })
 }
